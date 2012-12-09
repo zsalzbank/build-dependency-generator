89faaf4d3632fd042959f1ff061a5268886473e5
Zachary Salzbank <zsalzbank@gmail.com>
2012-12-08 23:19:39 -0500

    updates

--- run.sh
+++ run.sh
@@ -10,10 +10,6 @@ done
 strace -f -o strace.txt $CMD
 python "$DIR/depread.py" strace.txt > deps.txt
 python "$DIR/depgen.py" deps.txt > tree.json
-cp tree.json $DIR
 
-cd $DIR
-python -m SimpleHTTPServer &
-PID=$!
-chromium-browser "http://localhost:8000/display.htm?js=tree.json"
-kill -9 $PID
+bash "$DIR/display.sh"
+
