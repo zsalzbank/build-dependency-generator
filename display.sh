d6fa3a95b62344d64407d89e6b6f8ef7402381a7
Zachary Salzbank <zsalzbank@gmail.com>
2012-12-08 23:19:39 -0500

    updates

---
+++ display.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cp tree.json $DIR
+
+cd $DIR
+python -m SimpleHTTPServer &
+PID=$!
+chromium-browser "http://localhost:8000/display.htm?js=tree.json"
+sleep 10
+kill -9 $PID
