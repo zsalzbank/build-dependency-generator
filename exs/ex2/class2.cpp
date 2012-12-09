abef3a56f5642a9097671e17ed303eca0d3cf7d7
Zachary Salzbank <zsalzbank@gmail.com>
2012-12-09 17:51:01 -0500

    examples

---
+++ exs/ex2/class2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "headers.h"
+
+class2::class2() {
+    std::cout << "Hello from class2!" << std::endl;
+}
---
+++ exs/ex2/headers.h
@@ -0,0 +1,8 @@
+class class1 {
+public:
+    class1();
+};
+class class2 {
+public:
+    class2();
+};
